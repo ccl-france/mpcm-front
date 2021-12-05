190a8cd5962c0d83af07e1e7e232c1f2af4603e9
lodi-g <notifications@lodi.me>
2021-12-05 19:57:12 +0100

    Add staging Docker image and related CI
    Also parallelizes prod CI
    Reusable CI files
    
    Closes #8

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:16-alpine
+
+EXPOSE 3000
+WORKDIR /opt
+
+COPY package* ./
+RUN npm ci
+
+COPY . ./
+
+CMD npm run dev
--- src/const.ts
+++ src/const.ts
@@ -4,10 +4,11 @@ import FaTwitterSquare from "svelte-icons/fa/FaTwitterSquare.svelte";
 import FaEnvelope from "svelte-icons/fa/FaEnvelope.svelte";
 
 export const siteUrl = "https://lemeilleurplanclimatdumonde.fr";
-export const apiUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://api.lemeilleurplanclimatdumonde.fr"
-    : "http://localhost:1337";
+export const apiUrl = process.env.API_URL
+  ? process.env.API_URL
+  : process.env.NODE_ENV === "production"
+  ? "https://api.lemeilleurplanclimatdumonde.fr"
+  : "http://localhost:1337";
 
 export const pages = [
   ["/", "Accueil"],
@@ -28,14 +29,12 @@ export const shareButtons = [
     text: "Facebook",
   },
   {
-    href:
-      "https://twitter.com/share?url=%s&text=Un%20revenu%20climatique%20socialement%20juste,%20",
+    href: "https://twitter.com/share?url=%s&text=Un%20revenu%20climatique%20socialement%20juste,%20",
     icon: FaTwitterSquare,
     text: "Twitter",
   },
   {
-    href:
-      "mailto:??subject=Proposition citoyenne climatique&body=Bonjour,%0D%0A%0D%0AJe t'invite a regarder cette proposition citoyenne : ",
+    href: "mailto:??subject=Proposition citoyenne climatique&body=Bonjour,%0D%0A%0D%0AJe t'invite a regarder cette proposition citoyenne : ",
     icon: FaEnvelope,
     text: "E-mail",
   },
